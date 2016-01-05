5ea143ad5bf4d0c9e58bfb5856dc67aa2f8b3b41
Peter Hallam <peter@peterhallam.com>
2016-01-05 14:02:30 -0800

    Add bin/sabor.js

---
+++ bin/sabor.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+require('babel-register');
+require('babel-polyfill');
+require('es6-promise').polyfill();
+
+// Takes a list of JS files as arguments.
+// Traverses all relative imports and require statements.
+// Detects and reports any cycles between JS files.
+// Exit code 1 if any cycles were detected.
+require('../src/main');
